24bfbf5733e38a787060d8f7caf230a9f48e89c4
ION28 <jtsmith282@gmail.com>
2018-03-24 17:15:59 -0400

    Fixes on config for headless var, Add support for user setup, Closes #40

--- Packer/windows_x64/scripts/add-users.ps1
+++ Packer/windows_x64/scripts/add-users.ps1
@@ -1,17 +1,44 @@
 ﻿# Script to add users to a computer with variety of options
-# Using net user commands for backwards compatability all the way back
-
+# Using net user commands for backwards compatability
 # CREATE USER
 <#
 /active:yes/no - enable/disable user account
 /passwordchg:yes/no - user can/cannot change password
-/domain - add to domain, not locally
 /comment:"Text" - set description of user
 /fullname:"Name" - set full name of user
 /random - give the user a random password
 #>
-net user /add username Password
-#wmic useraccount where "name='developer'" set PasswordExpires=false
+#Example: net user /add username Password
 
 # ADD USER TO GROUPS
-#net localgroup group_name developer /add
+#net localgroup group_name user_name /add
+
+$user_config_file = Get-Content -Path "A:\local_users.json" -Raw
+
+$users_json = $user_config_file | ConvertFrom-Json
+ForEach($user in ($users_json | select -expand local_users)) {
+    $add_cmd = "net user /add $($user.username) "
+    if($user.random -eq $true) {
+        $add_cmd += "/random "
+    } else {
+        $add_cmd += "$($user.password) "
+    }
+    if($user.active -eq $true) {
+        $add_cmd += "/active:yes "
+    } else {
+        $add_cmd += "/active:no "
+    }
+    if($user.passwordchg -eq $true) {
+        $add_cmd += "/passwordchg:yes "
+    } else {
+        $add_cmd += "/passwordchg:no "
+    }
+    $add_cmd += "/comment:`"$($user.comment)`" "
+    $add_cmd += "/fullname:`"$($user.fullname)`" "
+    iex $add_cmd
+
+    ForEach($group in $user.groups) {
+        iex "net localgroup `"$group`" $($user.username) /add"
+    }
+}
+
