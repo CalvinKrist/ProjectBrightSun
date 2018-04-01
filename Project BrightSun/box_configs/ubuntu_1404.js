5d09c11c11d00089737b99df047f79efe7ac2f50
CalvinKrist <calvin.krist@yahoo.com>
2018-04-01 13:10:56 -0400

    ubuntu automated download scripting support

---
+++ Project BrightSun/box_configs/ubuntu_1404.js
@@ -0,0 +1,12 @@
+var ubuntu_1404 = {
+  "vm_name": "ubuntu1404",
+  "cpus": "1",
+  "disk_size": "65536",
+  "iso_checksum": "dde07d37647a1d2d9247e33f14e91acb10445a97578384896b4e1d985f754cc1",
+  "iso_checksum_type": "sha256",
+  "iso_name": "ubuntu-14.04.5-server-amd64.iso",
+  "iso_url": "http://releases.ubuntu.com/14.04.5/ubuntu-14.04.5-server-amd64.iso",
+  "memory": "1024",
+  "preseed" : "preseed.cfg",
+  "boot_command_prefix": "<esc><esc><enter><wait>"
+}
---
+++ Project BrightSun/box_configs/ubuntu_1604.js
@@ -0,0 +1,11 @@
+var ubuntu_1604 = {
+  "vm_name": "ubuntu1604",
+  "cpus": "1",
+  "disk_size": "65536",
+  "iso_checksum": "0a03608988cfd2e50567990dc8be96fb3c501e198e2e6efcb846d89efc7b89f2",
+  "iso_checksum_type": "sha256",
+  "iso_name": "ubuntu-16.04.4-server-amd64.iso",
+  "iso_url": "http://releases.ubuntu.com/16.04.4/ubuntu-16.04.4-server-amd64.iso",
+  "memory": "1024",
+  "preseed" : "preseed.cfg"
+}
