bc11bd4b10888fd02879ce06a30e03be2c9e913c
Saeed Razavi <louisrazavi@gmail.com>
2018-04-21 23:07:44 -0400

    Edit modal is visually functional although the cards don't change when edited yet. The edit function is entirely functional technically. Closes #84

--- js/config_web_script.js
+++ js/config_web_script.js
@@ -53,8 +53,10 @@ $(function(){ //shorthand for $(document).ready(function(){...});
 			}
 		});
 		
+		//Duplicate of the above - clean up in future
 		//edit-modal: When the platform is changed in the drop-down, clears and repopulates os drop-down options
-		$('#editPlatformSelect').change(function() {
+
+		function updateOsDropdown(){	
 			if($( "#editPlatformSelect option:selected" ).val() !== 'default'){
 				osSelect.removeAttr('disabled');
 				if($("#editPlatformSelect option:selected").val()==="windows_x64"){
@@ -87,8 +89,9 @@ $(function(){ //shorthand for $(document).ready(function(){...});
 				// not functional should reset selection when dropdown is disabled $('#defaultOs').attr('selected','selected').siblings().removeAttr('selected');
 				osSelect.attr('disabled','disabled');
 			}
-		});
-		
+		}
+		$('#editPlatformSelect').change(updateOsDropdown);
+				
 		
 		//When "add" button on modal is clicked, checks if all fields are populated. If not,
 		//gives a warning saying to do so and lets you return to the modal to try again. 
@@ -102,6 +105,18 @@ $(function(){ //shorthand for $(document).ready(function(){...});
 			}
 		});
 		
+		//When "Save Edits" button on edit modal is clicked, checks if all fields are populated. If not,
+		//gives a warning saying to do so and lets you return to the modal to try again. 
+		//If so, hides the modal and calls the save_edits() function
+		$("#saveEditsButton").on('click', function(){
+			if(edit_check_all_add_fields_set()) {
+				$('#editModal').modal('hide');
+				save_edits();
+			} else {
+				$('#editModal').modal('show');
+			}
+		});
+		
 		
 		//checks if name field is populated with valid content. If not, returns false and gives an alert, otherwise, returns true.
 		function check_all_add_fields_set() {
@@ -109,6 +124,19 @@ $(function(){ //shorthand for $(document).ready(function(){...});
 				alert("Please give your box a hostname!");
 				return false;
 			} else if(window.boxes.map(value => value.name).includes($("#machineNameBox").val())) {
+				alert("Your box hostname cannot be the same as another box!");
+				return false;
+			}
+			return true;
+		}
+		
+		//Duplicate of the above - clean up in future 
+		//edit-modal- checks if name field is populated with valid content. If not, returns false and gives an alert, otherwise, returns true.
+		function edit_check_all_add_fields_set() {
+			if($("#editMachineNameBox").val() === "") { //name not set
+				alert("Please give your box a hostname!");
+				return false;
+			} else if($("#editMachineNameBox").val() !== $("#editModal").data("currentMachName") && window.boxes.map(value => value.name).includes($("#editMachineNameBox").val())) {
 				alert("Your box hostname cannot be the same as another box!");
 				return false;
 			}
@@ -145,14 +173,41 @@ $(function(){ //shorthand for $(document).ready(function(){...});
 			
 			var os_label = '<p>' + os + '</p>';
 			
-			$($('#card_well').last().children().last().children().last())[0].innerHTML += '<div class="card"><div class="card-body"><h5 class="card-title">' + machName + '</h5><p class="card-text"><table class="table"><tbody><tr><td>Platform:</td><td>' + plat_label + '</td></tr><tr><td>Operating System:</td><td>' + os_label + '</td></tr></table></p></div><div class="card-footer"><a href="#" class="card-link btn btn-sm btn-info editButton" data-toggle="modal" data-target="#editModal">Edit</a><a href="#" class="card-link btn btn-sm btn-danger removeButton">Remove</a></div></div>';
+			$($('#card_well').last().children().last().children().last())[0].innerHTML += '<div class="card" data-name="'+machName+'" data-platform = "'+plat+'" data-os = "'+os+'"><div class="card-body"><h5 class="card-title">' + machName + '</h5><p class="card-text"><table class="table"><tbody><tr><td>Platform:</td><td>' + plat_label + '</td></tr><tr><td>Operating System:</td><td>' + os_label + '</td></tr></table></p></div><div class="card-footer"><a href="#" class="card-link btn btn-sm btn-info editButton" data-toggle="modal" data-target="#editModal">Edit</a><a href="#" class="card-link btn btn-sm btn-danger removeButton">Remove</a></div></div>';
+		}
+		
+		//saves data selected in modal to the current box and card being edited
+		function save_edits(){
+			var machName = $("#editMachineNameBox").val();
+			var plat = $("#editPlatformSelect option:selected").val();
+			var os = $("#editOsSelect option:selected").val();
+
+			
+			//find the right box and change the content in it
+			
+			var machToEditIndex;
+			$.each(window.boxes, function(index, value){
+				if($('#editModal').data("currentMachName") === value.name){
+					machToEditIndex = index;
+					return false;
+				}
+			});
+	
+			window.boxes[machToEditIndex].name = machName;
+			window.boxes[machToEditIndex].platform = plat;
+			window.boxes[machToEditIndex].os_version = os;
+		
+			console.log(machToEditIndex+"\n"+window.boxes[machToEditIndex].name+"\n"+window.boxes[machToEditIndex].platform+"\n"+window.boxes[machToEditIndex].os_version);
+			
+			//find the right card and change the inner HTML
+				//change the html data and the internal labels
 		}
 		
 		
 		//removes the object represented by the card from window.boxes then removes the card from the GUI
 		$('#card_well').on('click', '.removeButton', function(event){   			//must use  $('#card_well').on('click', '.removeButton', function(event){ instead of $('.removeButton').on('click', function(event){ because you can only directly target elements that exist when the script it initially executed
 
-			var machToRemoveName = $(event.currentTarget).closest('.card').find($('.card-title')).text();
+			var machToRemoveName = $(event.currentTarget).closest('.card').data("name");
 			var machToRemoveIndex;
 			$.each(window.boxes, function(index, value){
 				if(machToRemoveName === value.name){
@@ -167,14 +222,18 @@ $(function(){ //shorthand for $(document).ready(function(){...});
 		
 		
 		$('#card_well').on('click', '.editButton', function(event){
-			var machName = $(event.currentTarget).closest('.card').find($('.card-title')).text();
-			var machPlat = $(event.currentTarget).closest('.card').find($('#platformSelect option:selected')).val();
-			var machOs = $(event.currentTarget).closest('.card').find($('#psSelect option:selected')).val();
-		
+			var machName = $(event.currentTarget).closest('.card').data("name");
+			var machPlat = $(event.currentTarget).closest('.card').data("platform");
+			var machOs = $(event.currentTarget).closest('.card').data("os");
+
 			$('#editMachineNameBox').val(machName);
 			$('#editPlatformSelect').val(machPlat);
+			updateOsDropdown();
 			$('#editOsSelect').val(machOs);
 			
+			$('#editModal').data("currentMachName", machName);
+			$('#editModal').data("currentMachPlat", machPlat);
+			$('#editModal').data("currentMachOs", machOs);
 		});
 		
 		
