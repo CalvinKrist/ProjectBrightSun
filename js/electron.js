60d61c16852a052fef6220c193ef45bff4e59fd7
CalvinKrist <calvin.krist@yahoo.com>
2018-07-12 07:46:44 -0400

    Minor formatting changes.

--- js/electron.js
+++ js/electron.js
@@ -1,12 +1,13 @@
-function isDescendant(parent, child) {
-     var node = child.parentNode;
-     while (node != null) {
-         if (node == parent) {
-             return true;
-         }
-         node = node.parentNode;
-     }
-     return false;
+/**
+
+This script file contains general utility functions that may need to be available from any webView.
+
+*/
+
+function loadFile(filePath) {
+	var fileReader = require('fs');
+	
+	return fileReader.readFileSync(filePath).toString();
 }
 
 // Used to load a 'View': a 'view' is a section of HTML that is then inserted into the
@@ -14,39 +15,31 @@ function isDescendant(parent, child) {
 // index.html as well if they don't already exist.
 
 //TODO: add ability to manually specify that a script tag should not be inserted into index.html
-function loadWebView(webPage) {
+function loadWebView(webView) {
+	// Set innerHTML for content-view
+	document.getElementById("content-view").innerHTML = loadFile(webView);
 	
-	var fileReader = require('fs');
-	fileReader.readFile(webPage, function (err, data) {
-		if (err) {
-			throw err; 
+	// Clear footer
+	document.getElementById("document-footer").innerHTML = "";
+	
+	// Add new script tags in order to dynamically execute JS and import JS
+	var scriptTags = document.getElementById("content-view").getElementsByTagName('script');
+	
+	for(var i = 0; i < scriptTags.length; i++) {
+		var script = document.createElement('script');
+		if(scriptTags[i].src != null)
+			script.src = scriptTags[i].src;
+		if(scriptTags[i].id != null)
+			script.id = scriptTags[i].id;
+		if(scriptTags[i].className != null)
+			script.className = scriptTags[i].className;
+		if(scriptTags[i].innerHTML != null) {
+			script.innerHTML = scriptTags[i].innerHTML;
+			eval(script.innerHTML);
 		}
 		
-		// Set innerHTML for content-view
-		document.getElementById("content-view").innerHTML = data.toString();
-		
-		// Clear footer
-		document.getElementById("document-footer").innerHTML = "";
-		
-		// Add new script tags in order to dynamically execute JS and import JS
-		var scriptTags = document.getElementById("content-view").getElementsByTagName('script');
-		
-		for(var i = 0; i < scriptTags.length; i++) {
-			var script = document.createElement('script');
-			if(scriptTags[i].src != null)
-				script.src = scriptTags[i].src;
-			if(scriptTags[i].id != null)
-				script.id = scriptTags[i].id;
-			if(scriptTags[i].className != null)
-				script.className = scriptTags[i].className;
-			if(scriptTags[i].innerHTML != null) {
-				script.innerHTML = scriptTags[i].innerHTML;
-				eval(script.innerHTML);
-			}
-			
-			scriptTags[i].parentNode.replaceChild(script, scriptTags[i]);
-		}
-	});
+		scriptTags[i].parentNode.replaceChild(script, scriptTags[i]);
+	}
 }
 
 function setCurrentNavPage(currentNavPageLinkId) {
